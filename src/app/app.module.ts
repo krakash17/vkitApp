02c77b14aa260d42b0ec2af7fd1e670e170ed76c
krakash17 <akashkr1797@gmail.com>
2019-09-23 22:16:57 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { RouterModule, Routes } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
@@ -9,11 +9,31 @@ import { HomeComponent } from './pages/home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPage} from './pages/login/login.component';
+import { CoursesComponent} from './pages/courses/courses.component';
+import { MatTabsModule,MatExpansionModule,MatIconModule } from '@angular/material';
+import {MatGridListModule} from '@angular/material/grid-list';
+
+import { FormsModule } from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'courses', component: CoursesComponent },
+
+  { path: 'login', component: LoginPage },
+  { path: '', component: LoginPage },
+
+]
 
 @NgModule({
-  declarations: [AppComponent,HomeComponent,LoginPage],
+  declarations: [AppComponent,HomeComponent,LoginPage,CoursesComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule,MatGridListModule,MatExpansionModule, MatIconModule,IonicModule.forRoot(),MatTabsModule, 
+    BrowserAnimationsModule, AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } 
+    )
+  ],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,27 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { MenuController } from '@ionic/angular';
+import { Component, OnInit, } from '@angular/core';
+import { MenuController,IonicModule} from '@ionic/angular';
+import { MatTabsModule } from '@angular/material';
+import { ActivatedRoute,Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor(private menu: MenuController) { }
+  
+  constructor(public menu: MenuController,
+    private route:ActivatedRoute,private router:Router) { }
 
   ngOnInit() {}
 
-  openFirst() {
-    this.menu.enable(true, 'first');
-    this.menu.open('first');
+  slideOpts = {
+    
+    speed: 400,
+    initialSlide: 0,
+    slidesPerView: 1,
+    autoplay:true
+  };
+  goToCourses(){
+    this.router.navigateByUrl('/courses')
   }
+  
+  
 
-  openEnd() {
-    this.menu.open('end');
-  }
-
-  openCustom() {
-    this.menu.enable(true, 'custom');
-    this.menu.open('custom');
-  }
+ 
 }
